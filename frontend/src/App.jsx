b1af817df9d47f4f4b242e9d86796c4046b13a6c
1romang1 <krglrv@gmail.com>
2026-02-11 17:40:32 +0300

    create routing fix #25

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from "./Components/Pages/MainPage";
+import LoginPage from "./Components/Pages/LoginPage";
+import NotFoundPage from "./Components/Pages/NotFoundPage";
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
