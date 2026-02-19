188bd85c40d5ba7ea9b80db31cbcfa9992a83708
1romang1 <krglrv@gmail.com>
2026-02-19 15:14:49 +0300

    added validation in avtorisation form fix 31#

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,35 +1,58 @@
-import { useFormik } from "formik";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 const LoginForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      userName: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
-    },
-  });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="userName">User name</label>
-      <input
-        id="userName"
-        name="userName"
-        type="userName"
-        onChange={formik.handleChange}
-        value={formik.values.userName}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <Formik
+        initialValues={{
+          userName: "",
+          password: "",
+        }}
+        validationSchema={Yup.object({
+          userName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          password: Yup.string()
+            .required("Required")
+            .min(8, "Minimum of 8 characters"),
+        })}
+        onSubmit={(values) => {
+          console.log(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="User Name"
+            name="userName"
+            type="text"
+            placeholder="Ivan"
+          />
+
+          <MyTextInput
+            label="Password"
+            name="password"
+            type="text"
+            placeholder="**********"
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
