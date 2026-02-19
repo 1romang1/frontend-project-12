efc1c281e5c4f016368b51a2bc2e6ad914f55cf0
1romang1 <krglrv@gmail.com>
2026-02-19 11:16:50 +0300

    added LoginForm fix #30

---
+++ frontend/src/Components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import { useFormik } from "formik";
+
+const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      userName: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="userName">User name</label>
+      <input
+        id="userName"
+        name="userName"
+        type="userName"
+        onChange={formik.handleChange}
+        value={formik.values.userName}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default LoginForm;
