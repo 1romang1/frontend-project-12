b1af40f7c0ccdc166c6bf137e0b3e679f4e87e66
1romang1 <krglrv@gmail.com>
2026-03-26 14:49:39 +0300

    optimization baseQuery fix #59

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,15 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const baseQuery = fetchBaseQuery({
-  baseUrl: "/api/v1",
-  prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQuery } from "./baseQuery";
 
 export const authApi = createApi({
   reducerPath: "authApi",
