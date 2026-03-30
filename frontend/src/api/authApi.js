34b4503685ab25b601c221a57920be23fd079f6e
1romang1 <krglrv@gmail.com>
2026-03-30 11:35:11 +0300

    Revert "optimization baseQuery fix #59"
    This reverts commit b1af40f7c0ccdc166c6bf137e0b3e679f4e87e66.

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,5 +1,15 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import { baseQuery } from "./baseQuery";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "/api/v1",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const authApi = createApi({
   reducerPath: "authApi",
