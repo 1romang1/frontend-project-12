7428757f8c92e17aa3f21deb636530c6060fab6b
1romang1 <krglrv@gmail.com>
2026-03-30 12:34:32 +0300

    added reducers and middlewares in store fix #55

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "../api/authApi";
 import { authReducer } from "./slices/authSlice";
+import { channelsApi } from "../api/channelsApi";
+import { channelsReducer } from "./slices/channelsSlice";
+import { messagesApi } from "../api/messagesApi";
+import { messagesReducer } from "./slices/messagesSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    channels: channelsReducer,
+    messages: messagesReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesReducer,
   },
   middleware: (getDefaultMiddlware) =>
-    getDefaultMiddlware().concat(authApi.middleware),
+    getDefaultMiddlware().concat(
+      authApi.middleware,
+      channelsApi.middleware,
+      messagesApi.middleware,
+    ),
 });
