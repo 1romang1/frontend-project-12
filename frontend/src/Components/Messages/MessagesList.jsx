0bff2810848b3c1ed3c222bec0572e1ac995fb22
1romang1 <krglrv@gmail.com>
2026-04-08 12:43:42 +0300

    WIP: доделать MessagesList & MessagesItem

--- frontend/src/Components/Messages/MessagesList.jsx
+++ frontend/src/Components/Messages/MessagesList.jsx
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setMessages } from "../../store/slices/messagesSlice";
 import { useSelector } from "react-redux";
-
+import { selectCurrentChannelMessages } from "../currentChannelMessagesSelector";
 import { useGetMessagesQuery } from "../../api/messagesApi";
+import { selectCurrentChannelId } from "../currentChannelMessagesSelector";
+import { Message } from "./MessageItem";
 
 export const MessagesList = () => {
-    const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-    const { data: messages = [] } = useGetMessagesQuery();
+    console.log(useGetMessagesQuery);
+  const dispatch = useDispatch();
+  const { data: messages = [] } = useGetMessagesQuery();
+  console.log(messages)
+const currentChannelMessages = useSelector(selectCurrentChannelMessages);
+  useEffect(() => {
+    dispatch(setMessages(messages));
+    
+  }, [messages, dispatch]);
+
+  const currentChannelId = useSelector(selectCurrentChannelId);
+//   const currentChannelMessages = useSelector(selectCurrentChannelMessages);
+
+  return (
+    <div className="d-flex flex-column h-100 flex-grow-1">
+      <div className="bg-light mb-4 p-3 shadow-sm small">
+        <p className="m-0">
+          <b># general</b>
+        </p>
+        <span className="text-muted">1 сообщение</span>
+      </div>
+
+      <div className="chat-messages overflow-auto px-5">
+        {currentChannelMessages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+    </div>
+    
+  );
+};
