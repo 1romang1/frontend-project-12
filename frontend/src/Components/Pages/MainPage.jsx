3050b22e8a552f7742b36bd42d0e70c2b613e513
1romang1 <krglrv@gmail.com>
2026-04-08 12:37:54 +0300

    added MessageList in MainPage

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import { ChannelList } from "../Channels/ChannelList";
+import { MessagesList } from "../Messages/MessagesList";
 
 const MainPage = () => {
     return (
         <div>
             Main Page
             <ChannelList />
+            <MessagesList />
         </div>
     );
 };
