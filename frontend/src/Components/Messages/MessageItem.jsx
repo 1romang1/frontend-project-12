6cdfbfe8ccdc0630718c57c872f41f90e2107614
1romang1 <krglrv@gmail.com>
2026-04-10 15:47:46 +0300

    added container-components & presentational-components fix #61

--- frontend/src/Components/Messages/MessageItem.jsx
+++ frontend/src/Components/Messages/MessageItem.jsx
@@ -1,4 +1,4 @@
-export const Message = ({ message }) => {
+export const MessageItem = ({ message }) => {
   return (
     <div className="text-break mb-2">
       <b>{message.username}</b>: {message.body}
--- frontend/src/Components/Messages/MessagesList.jsx
+++ frontend/src/Components/Messages/MessagesList.jsx
@@ -2,24 +2,28 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setMessages } from "../../store/slices/messagesSlice";
 import { useSelector } from "react-redux";
-import { selectCurrentChannelMessages } from "../currentChannelMessagesSelector";
+// import { selectCurrentChannelMessages } from "../currentChannelMessagesSelector";
 import { useGetMessagesQuery } from "../../api/messagesApi";
-import { selectCurrentChannelId } from "../currentChannelMessagesSelector";
-import { Message } from "./MessageItem";
+// import { selectCurrentChannelId } from "../currentChannelMessagesSelector";
+import { MessageItem } from "./MessageItem";
+import { setCurrentChannelId } from "../../store/slices/channelsSlice";
 
 export const MessagesList = () => {
-    console.log(useGetMessagesQuery);
   const dispatch = useDispatch();
+
   const { data: messages = [] } = useGetMessagesQuery();
-  console.log(messages)
-const currentChannelMessages = useSelector(selectCurrentChannelMessages);
+
+  const currentChannelId = useSelector(
+    (state) => state.channels.currentChannelId,
+  );
+
   useEffect(() => {
     dispatch(setMessages(messages));
-    
   }, [messages, dispatch]);
 
-  const currentChannelId = useSelector(selectCurrentChannelId);
-//   const currentChannelMessages = useSelector(selectCurrentChannelMessages);
+  const currentChannelMessages = messages.filter(
+    (m) => m.channelId === currentChannelId
+  );
 
   return (
     <div className="d-flex flex-column h-100 flex-grow-1">
@@ -36,6 +40,5 @@ const currentChannelMessages = useSelector(selectCurrentChannelMessages);
         ))}
       </div>
     </div>
-    
   );
 };
