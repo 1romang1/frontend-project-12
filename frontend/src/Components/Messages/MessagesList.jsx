fad3fcb38d459cad3a4a5b731b71f20dcb04e0b4
Roma <krglrv@gmail.com>
2026-04-11 14:53:21 +0300

    deleted everything related to messages slice fix #67

--- frontend/src/Components/Messages/MessagesList.jsx
+++ frontend/src/Components/Messages/MessagesList.jsx
@@ -1,28 +1,16 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setMessages } from "../../store/slices/messagesSlice";
 import { useSelector } from "react-redux";
-// import { selectCurrentChannelMessages } from "../currentChannelMessagesSelector";
 import { useGetMessagesQuery } from "../../api/messagesApi";
-// import { selectCurrentChannelId } from "../currentChannelMessagesSelector";
 import { MessageItem } from "./MessageItem";
-import { setCurrentChannelId } from "../../store/slices/channelsSlice";
 
 export const MessagesList = () => {
-  const dispatch = useDispatch();
-
   const { data: messages = [] } = useGetMessagesQuery();
 
   const currentChannelId = useSelector(
     (state) => state.channels.currentChannelId,
   );
 
-  useEffect(() => {
-    dispatch(setMessages(messages));
-  }, [messages, dispatch]);
-
   const currentChannelMessages = messages.filter(
-    (m) => m.channelId === currentChannelId
+    (m) => m.channelId === currentChannelId,
   );
 
   return (
@@ -36,7 +24,7 @@ export const MessagesList = () => {
 
       <div className="chat-messages overflow-auto px-5">
         {currentChannelMessages.map((message) => (
-          <Message key={message.id} message={message} />
+          <MessageItem key={message.id} message={message} />
         ))}
       </div>
     </div>
--- frontend/src/Components/currentChannelMessagesSelector.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-export const selectAllMessages = (state) => state.messages.items;
-export const selectCurrentChannelId = (state) => state.channels.currentChannelId;
-
-export const selectCurrentChannelMessages = createSelector(
-    [selectAllMessages, selectCurrentChannelId],
-    (messages, currentChannelId) => messages.filter((message) => message.channelId === currentChannelId)
-);
