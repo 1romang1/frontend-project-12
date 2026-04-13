b8285a8c6939a4cc70affba2a8819cd62ef04b82
Roma <krglrv@gmail.com>
2026-04-13 23:44:02 +0300

    WIP: finish issue#66

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,14 +1,26 @@
+import { Container, Row, Col } from "react-bootstrap";
 import { ChannelList } from "../Channels/ChannelList";
 import { MessagesList } from "../Messages/MessagesList";
 
 const MainPage = () => {
-    return (
-        <div>
-            Main Page
-            <ChannelList />
-            <MessagesList />
+  return (
+    <div className="h-100 bg-light">
+      <div className="h-100">
+        <div className="h-100" id="chat">
+          <Container fluid className="h-100">
+            <Row className="h-100">
+              <Col xs={3} className="border-end px-0">
+                <ChannelList />
+              </Col>
+              <Col xs={9} className="d-flex flex-column px-0">
+                <MessagesList />
+              </Col>
+            </Row>
+          </Container>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
