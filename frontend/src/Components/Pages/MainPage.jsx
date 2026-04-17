0a574d5668b61c222b51edbbc1516b44bcbd9bc4
1romang1 <krglrv@gmail.com>
2026-04-17 12:46:08 +0300

    updated the MainPage markup

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -4,19 +4,25 @@ import { MessagesList } from "../Messages/MessagesList";
 
 const MainPage = () => {
   return (
-    <div className="h-100 bg-light">
+    <div className="vh-100 d-flex flex-column bg-light">
       <div className="h-100">
         <div className="h-100" id="chat">
-          <Container fluid className="h-100">
-            <Row className="h-100">
-              <Col xs={3} className="border-end px-0">
-                <ChannelList />
-              </Col>
-              <Col xs={9} className="d-flex flex-column px-0">
-                <MessagesList />
-              </Col>
-            </Row>
-          </Container>
+          <div className="d-flex flex-column h-100">
+            <Container className="h-100 my-4 overflow-hidden rounded shadow">
+              <Row className="h-100 bg-white flex-md-row">
+                <Col
+                  md={2}
+                  xs={4}
+                  className="border-end px-0 bg-light d-flex flex-column h-100"
+                >
+                  <ChannelList />
+                </Col>
+                <Col className="p-0 h-100">
+                  <MessagesList />
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </div>
       </div>
     </div>
