8e11d80ab1c2f1d8ca48c6907fee2ad8a8847fb8
1romang1 <krglrv@gmail.com>
2026-05-15 16:06:48 +0300

    fixed username

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -4,13 +4,14 @@ const authSlice = createSlice({
   name: "auth",
   initialState: {
     token: localStorage.getItem("token"),
-    username: null,
+    username: localStorage.getItem("username"),
   },
   reducers: {
     setCredentials: (state, action) => {
       state.token = action.payload.token;
       state.username = action.payload.username
       localStorage.setItem("token", state.token);
+      localStorage.setItem("username", state.username);
     },
     logout: (state) => {
       state.token = null;
