fc3d7a41c9ee0d9b0d8e5cd333752a79bfa97d57
Roma <krglrv@gmail.com>
2026-05-21 22:37:18 +0300

    WIP: RegistrationForm

--- frontend/src/Components/RegistrationForm.jsx
+++ frontend/src/Components/RegistrationForm.jsx
@@ -3,70 +3,74 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { setCredentials } from "../store/slices/authSlice";
-import { useSignupMutation } from "../api/signupApi"; 
+import { useSignupMutation } from "../api/signupApi";
 
 const MyTextInput = ({ label, ...props }) => {
-    const [field, meta] = useField(props);
-    return (
-        <>
-            <label htmlFor={props.id || props.name}>{label}</label>
-            <input className="text-input" {...field} {...props} />
-            {meta.touched && meta.error ? (
-                <div className="error">{meta.error}</div>
-            ) : null}
-        </>
-    );
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
 };
 
 const RegistrationForm = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [login, { isError }] = useLoginMutation();
-    return (
-        <>
-            <Formik
-                initialValues={{
-                    username: "",
-                    password: "",
-                }}
-                validationSchema={Yup.object({
-                    username: Yup.string()
-                        .max(15, "Must be 15 characters or less")
-                        .required("Required"),
-                    password: Yup.string()
-                        .required("Required")
-                        .min(5, "Minimum of 5 characters"),
-                })}
-                onSubmit={async (values) => {
-                    try {
-                        const response = await login(values).unwrap();
-                        dispatch(setCredentials(response));
-                        navigate("/");
-                    } catch (error) {
-                        console.error("Login error", error);
-                    }
-                }}
-            >
-                <Form>
-                    <MyTextInput
-                        label="User Name"
-                        name="username"
-                        type="text"
-                        placeholder="Ivan"
-                    />
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [signup] = useSignupMutation();
+  return (
+    <>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+          passwordConfirmation: "",
+        }}
+        validationSchema={Yup.object({
+          username: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          password: Yup.string()
+            .required("Required")
+            .min(5, "Minimum of 5 characters"),
+          passwordConfirmation: Yup.string()
+            .required("Required")
+            .oneOf([Yup.ref("password")], "Passwords must match"),
+        })}
+        onSubmit={async (values) => {
+          try {
+            const response = await signup(values).unwrap();
+            dispatch(setCredentials(response));
+            // navigate("/");
+          } catch (error) {
+            console.error("Login error", error);
+          }
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="User Name"
+            name="username"
+            type="text"
+            placeholder="Ivan"
+          />
 
-                    <MyTextInput
-                        label="Password"
-                        name="password"
-                        type="text"
-                        placeholder="**********"
-                    />
-                    {isError && <div>Неверный логин или пароль</div>}
-                    <button type="submit">Submit</button>
-                </Form>
-            </Formik>
-        </>
-    );
+          <MyTextInput
+            label="Password"
+            name="password"
+            type="text"
+            placeholder="**********"
+          />
+          {isError && <div>Неверный логин или пароль</div>}
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default RegistrationForm;
