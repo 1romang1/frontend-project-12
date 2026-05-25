70abe0f74af16344238caa786cd766a797d02e8a
1romang1 <krglrv@gmail.com>
2026-05-25 17:09:13 +0300

    general channel is default channel

--- frontend/src/Components/Channels/ChannelList.jsx
+++ frontend/src/Components/Channels/ChannelList.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useSelector } from 'react-redux';
 import ChannelItem from "./ChannelItem";
 import { useGetChannelsQuery } from "../../api/channelsApi";
 import { setCurrentChannelId } from "../../store/slices/channelsSlice";
@@ -8,9 +9,16 @@ export const ChannelList = () => {
   const { data: channels = [], isSuccess } = useGetChannelsQuery();
   const dispatch = useDispatch();
 
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
   useEffect(() => {
+    const generalChannel = channels.find(channel => channel.name === 'general');
+
+    if (isSuccess && channels.length > 0 && currentChannelId === undefined) {
+      dispatch(setCurrentChannelId({ id: generalChannel.id }));
+    }
     if (isSuccess && channels.length > 0) {
-      dispatch(setCurrentChannelId({id: channels[0].id}));
+      dispatch(setCurrentChannelId({ id: channels[0].id }));
     }
   }, [isSuccess, channels, dispatch]);
 
