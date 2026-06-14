c6a2034d2b316a81fd20fcfb279d2b42afaf0f41
Roma <krglrv@gmail.com>
2026-06-14 07:57:14 +0300

    WIP: update channelsApi

--- frontend/src/api/channelsApi.js
+++ frontend/src/api/channelsApi.js
@@ -2,13 +2,31 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQuery } from "./baseQuery";
 
 export const channelsApi = createApi({
-    reducerPath: 'channelsApi',
-    baseQuery,
-    endpoints: (builder) => ({
-        getChannels: builder.query({
-            query: () => 'channels'
-        })
-    })
-})
+  reducerPath: "channelsApi",
+  baseQuery,
+  endpoints: (builder) => ({
+    getChannels: builder.query({
+      query: () => "channels",
+    }),
+    createChannel: builder.mutation({
+      query: (data) => ({
+        url: "channels",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    renameChannel: builder.query({
+      mutation: (data) => ({
+        url: `channels/${data.id}`,
+        method: "PATCH",
+        body: data.name,
+      }),
+    }),
+  }),
+});
 
-export const { useGetChannelsQuery } = channelsApi;
+export const {
+  useGetChannelsQuery,
+  useCreateChannelMutation,
+  useRenameChannelMutation,
+} = channelsApi;
